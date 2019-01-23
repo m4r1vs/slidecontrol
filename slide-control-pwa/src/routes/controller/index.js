eb8d06dcfc41813c52c948ab4c982a38c93cffb5
Marius Niveri <marius.niveri@gmail.com>
2019-01-23 21:14:55 +0100

    added input and submit

--- slide-control-pwa/src/routes/controller/index.js
+++ slide-control-pwa/src/routes/controller/index.js
@@ -3,8 +3,8 @@ import style from './style.scss';
 
 export default class Profile extends Component {
 
-	componentDidMount() {
-		console.log('mounted');
+	componentWillMount(props) {
+		console.log('mounted', this.props.id);
 	}
 	
 	render({ id }) {
