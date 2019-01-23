940541ba390402d4bad986b432d145d3605d8604
Marius Niveri <marius.niveri@gmail.com>
2019-01-23 22:05:16 +0100

    Remote for slides code uploaded

---
+++ remote for slides/welcome-analytics.js
@@ -0,0 +1,11 @@
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function(...args) {
+        (i[r].q = i[r].q || []).push(args);
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1; a.src = g; m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-130575482-2', 'auto');
+ga('set', 'checkProtocolTask', function() { });
+ga('require', 'displayfeatures');
+ga('send', 'pageview', '/welcome.html');
