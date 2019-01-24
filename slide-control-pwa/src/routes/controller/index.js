a198f74793ede1f6807447358cc7f6fb5bfad860
Marius Niveri <marius.niveri@gmail.com>
2019-01-25 00:12:35 +0100

    added buttons to pwa

--- slide-control-pwa/src/routes/controller/index.js
+++ slide-control-pwa/src/routes/controller/index.js
@@ -2,24 +2,43 @@ import { h, Component } from 'preact';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+import Button from '../../components/buttons';
+
 import style from './style.scss';
 
 export default class Profile extends Component {
-
-	constructor(props) {
-		super(props);
-	}
-
+	
 	nextSlide() {
 		const presentations = this.db.collection('presentations');
 		presentations.doc(this.props.id).get()
 			.then(doc => {
-				console.log(doc.data());
+				presentations.doc(this.props.id).set({
+					activeSlide: doc.data().activeSlide + 1
+				});
 			});
 	}
 
-	componentWillMount(props) {
+	previousSlide() {
+		const presentations = this.db.collection('presentations');
+		presentations.doc(this.props.id).get()
+			.then(doc => {
+				presentations.doc(this.props.id).set({
+					activeSlide: doc.data().activeSlide - 1
+				});
+			});
+	}
+	
+	constructor(props) {
+
+		super(props);
+		
 		this.db = firebase.firestore();
+		this.nextSlide = this.nextSlide.bind(this);
+		this.previousSlide = this.previousSlide.bind(this);
+
+	}
+	
+	componentWillMount(props) {
 		const presentations = this.db.collection('presentations');
 		presentations.doc(this.props.id).get()
 			.then(doc => {
@@ -33,6 +52,8 @@ export default class Profile extends Component {
 		return (
 			<div class={style.profile}>
 				<h1 onClick={this.nextSlide}>Controller #{id}</h1>
+				<Button text="Previous" action={this.previousSlide} />
+				<Button text="Next" action={this.nextSlide} />
 			</div>
 		);
 	}
